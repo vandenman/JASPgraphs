e80863e0c74c5b0d7ccedc825bbbdaf17a8085e1
vandenman <donvdbergh@hotmail.com>
2021-06-16 16:36:55 +0200

    added action for pkgdown

--- R/functions.R
+++ R/functions.R
@@ -71,7 +71,7 @@ addAxis <- function(graph, breaks = NULL, name = waiver(), labels = waiver(), li
 
 }
 
-#' @title Deprecated: draw an empty ggplot with just axes.
+#' @title Deprecated: use ggplot2::ggplot in combination with ggplot2::scale_x|y_continuous.
 #'
 #' @param graph ggplot object.
 #' @param xName name for x-axis.
